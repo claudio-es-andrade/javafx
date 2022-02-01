395632aa4951899bfcf0e4a300d6cb7b675d01e2
Claudio ES Andrade <claudioe.s.andrade@gmail.com>
2022-02-01 17:05:08 -0300

    "Um simples Web Browser"

---
+++ WebBrowser/src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene.fxml"));
+			Parent root = loader.load();
+			
+			Controller controller = loader.getController();
+			Scene scene = new Scene(root);
+			// primaryStage.getIcons().add(new Image("yourImage.png"));
+			primaryStage.setTitle(" Claudio's Web browser");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
